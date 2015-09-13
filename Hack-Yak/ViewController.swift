edda8850a4c4b95aa71ed4c93486a81ba84148f3
Andrew Breckenridge <asbreckenridge@me.com>
2015-09-13 05:38:58 -0400

    more things

--- Hack-Yak/ViewController.swift
+++ Hack-Yak/ViewController.swift
@@ -17,17 +17,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    @IBAction func unwindToWelcomeScreen(segue: UIStoryboardSegue) {}
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
